94acf9df6b857732279d4b1e24bb88eb6f21e66c
Gilbert Lamphere gilbert.lamphere@gmail.com <gilbert.lamphere@gmail.com>
2017-09-18 13:49:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // write your code below!
 var name = "Susan"
+<<<<<<< HEAD
 var height = 39
 var message = `${name} is ${height} inches tall`
+=======
+var height = 74
+var message = ` is ${height} inches tall`
+>>>>>>> fa8587ad9be139e8cf9173df43c9e3e43db037e6
 
 module.exports = {name, height, message}
